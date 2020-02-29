bbf3da068342552ce1f9e03bb56eba28146b45e5
Jaana <jaana.seffer@khk.ee>
2020-02-29 13:37:50 +0200

    tabelite sidumine, kliendi nime lisamine

--- tabelite_yhendamine.php
+++ tabelite_yhendamine.php
@@ -1,12 +1,14 @@
 <?php include('config.php'); ?>
 <?php
-$paring = "SELECT arved.arve_nr, albumid.artist, albumid.album
+$paring = "SELECT arved.arve_nr, albumid.artist, albumid.album, kliendid.Eesnimi, kliendid.Perenimi
 			FROM arved
-			JOIN albumid ON arved.albumid_id=albumid.id";
+			INNER JOIN albumid ON arved.albumid_id=albumid.id
+			INNER JOIN kliendid ON arved.kliendid_id=kliendid.kliendid_id";
 $valjund = mysqli_query($yhendus, $paring);
 while($rida = mysqli_fetch_assoc($valjund)){
     echo 'Arve number: '.$rida['arve_nr'].'<br>';
-    echo 'Toote nimetus: '.$rida['artist'].'-'.$rida['album'].'<br><br>';
+    echo 'Toote nimetus: '.$rida['artist'].'-'.$rida['album'].'<br>';
+    echo 'Kliendi nimi: '.$rida['Eesnimi'].' '.$rida['Perenimi'].'<br><br>';
 }
 mysqli_free_result($valjund);
 mysqli_close($yhendus);
